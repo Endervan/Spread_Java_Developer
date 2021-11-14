197eb977860ad8020599c4fe31bc8d6b60d28b63
endva <endvan@gmail.com>
2021-11-14 07:43:11 -0300

    Fatorial

---
+++ loops_e_arrays/src/br/com/exercicios/arrays/Ex_Fatorial.java
@@ -0,0 +1,21 @@
+package br.com.exercicios.arrays;
+
+import java.util.Scanner;
+
+public class Ex_Fatorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scanner.nextInt();
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+        for (int i = fatorial; i >= 1; i--) {
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println(multiplicacao);
+    }
+}
