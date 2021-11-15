d99b24bed3b687afeb1b923e436c8060eaa1f5dd
endva <endvan@gmail.com>
2021-11-15 20:50:59 -0300

    Dama

--- loops_e_arrays/src/br/com/desafios/parEimpar.java
+++ loops_e_arrays/src/br/com/desafios/parEimpar.java
@@ -3,6 +3,19 @@ package br.com.desafios;
 import java.io.IOException;
 import java.util.Scanner;
 
+//Leia um valor inteiro N. Este valor será a quantidade de valores que serão lidos em seguida
+// Para cada valor lido, mostre uma mensagem em inglês dizendo se
+// este valor lido é par (EVEN), ímpar (ODD), positivo (POSITIVE)
+// ou negativo (NEGATIVE). No caso do valor ser igual a zero (0),
+// embora a descrição correta seja (EVEN NULL), pois por definição
+// zero é par, seu programa deverá imprimir apenas NULL.
+//
+//    Entrada
+//        A primeira linha da entrada contém um valor inteiro
+//        N(N < 10000) que indica o número de casos de teste.
+//        Cada caso de teste a seguir é um valor inteiro
+//        X (-107 < X <107).
+
 public class parEimpar {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
