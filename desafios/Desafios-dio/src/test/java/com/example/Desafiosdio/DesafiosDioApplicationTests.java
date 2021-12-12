b4a716f8b6fc2fa71e2be7d66bd47627193fe7c5
endva <endvan@gmail.com>
2021-12-12 13:36:55 -0300

    iniciando DesafiosDioApplication

---
+++ desafios/Desafios-dio/src/test/java/com/example/Desafiosdio/DesafiosDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Desafiosdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
